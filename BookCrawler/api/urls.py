a8c48842076c37b600129972d1de7572e5fea312
Hector Latorre <hlatorre@grouponlatam.com>
2019-10-11 16:36:47 -0300

    A very, very simple auth system, endpoint for creating users (only 10 allowed), new Postman collection -> endpoints now recieve POST request

--- BookCrawler/api/urls.py
+++ BookCrawler/api/urls.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from . import views
 
 urlpatterns = [
+    path('v1/user/create', views.create_user, name='create_user'),
     path('v1/tasks/scrap', views.tasks, name='tasks'),
     path('v1/categories/', views.get_categories, name='get_categories'),
     path('v1/book/id/<int:ide>', views.get_book, name='get_book_id'),
--- BookCrawler/api/views.py
+++ BookCrawler/api/views.py
@@ -3,11 +3,14 @@ from django.core import serializers
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+import json
+
 from .tasks import crawl_bookstore
 from .tasks import get_all_model_categories
 from .tasks import get_model_book_by_id
 from .tasks import get_model_book_by_upc
-
+from .tasks import create_api_user
+from .tasks import is_authenticated
 
 @csrf_exempt
 def tasks(request):
@@ -18,23 +21,42 @@ def tasks(request):
 
 @csrf_exempt
 def get_categories(request):
-    if request.method == 'GET':
+    auth_flag = is_authenticated(request)
+    if request.method == 'POST' and request.content_type == 'application/json' and auth_flag:
         categories = list(get_all_model_categories())
         return JsonResponse(categories, status=200, safe=False)
+    elif not auth_flag:
+        return JsonResponse({"error": "Wrong user credentials"}, status=401)
     else:
-        return JsonResponse({"error": "Wrong HTTP method."}, status=404)
+        return JsonResponse({"error": "Wrong HTTP method."}, status=405)
 
 @csrf_exempt
 def get_book(request, ide):
-    if request.method == 'GET' and 'upc' not in request.path:
+    auth_flag = is_authenticated(request)
+    if request.method == 'POST' and request.content_type == 'application/json' and 'upc' not in request.path and auth_flag:
         book = get_model_book_by_id(ide)
         return JsonResponse(book, status=200, safe=False)
-    elif request.method == 'GET':
+    elif request.method == 'POST' and request.content_type == 'application/json' and auth_flag:
         book = get_model_book_by_upc(ide)
         return JsonResponse(book, status=200, safe=False)
+    elif not auth_flag:
+        return JsonResponse({"error": "Wrong user credentials"}, status=401)
     else:
-        return JsonResponse({"error": "Bad request structure."}, status=404)
+        return JsonResponse({"error": "Bad request structure."}, status=405)
+
+@csrf_exempt
+def create_user(request):
+    if request.method == 'POST' and request.content_type == 'application/json':
+        user_data = json.loads(request.body.decode('utf-8'))
+        if 'username' not in user_data.keys() or 'password' not in user_data.keys():
+            return JsonResponse({"error": "Wrong JSON structure."}, status=406)
+        else:
+            result = create_api_user(user_data['username'], user_data['password'])
+            return JsonResponse({"result": str(result)}, status=201)
+
+    else:
+        return JsonResponse({"error": "Bad request"}, status=401)
 
 def _post_tasks():
     crawl_bookstore()
-    return JsonResponse({"message": "Scrapping started."}, status=302)
+    return JsonResponse({"message": "Scrapping started."}, status=202)
