21b2c55fa93bf0b31640a0bfa7115807a239d58b
Hector Latorre <hlatorre@grouponlatam.com>
2019-10-11 14:39:12 -0300

    Finished API, still missing auth before making calls, included postman collection

--- BookCrawler/api/urls.py
+++ BookCrawler/api/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('v1/tasks/scrap', views.tasks, name='tasks'),
+    path('v1/categories/', views.get_categories, name='get_categories'),
+    path('v1/book/id/<int:ide>', views.get_book, name='get_book_id'),
+    path('v1/book/upc/<ide>', views.get_book, name='get_book_upc'),
 ]
--- BookCrawler/api/views.py
+++ BookCrawler/api/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
+from django.core import serializers
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .tasks import crawl_bookstore
+from .tasks import get_all_model_categories
+from .tasks import get_model_book_by_id
+from .tasks import get_model_book_by_upc
 
 
 @csrf_exempt
@@ -11,6 +15,25 @@ def tasks(request):
         return _post_tasks()
     else:
         return JsonResponse({"error": "Wrong HTTP method."}, status=405)
+
+@csrf_exempt
+def get_categories(request):
+    if request.method == 'GET':
+        categories = list(get_all_model_categories())
+        return JsonResponse(categories, status=200, safe=False)
+    else:
+        return JsonResponse({"error": "Wrong HTTP method."}, status=404)
+
+@csrf_exempt
+def get_book(request, ide):
+    if request.method == 'GET' and 'upc' not in request.path:
+        book = get_model_book_by_id(ide)
+        return JsonResponse(book, status=200, safe=False)
+    elif request.method == 'GET':
+        book = get_model_book_by_upc(ide)
+        return JsonResponse(book, status=200, safe=False)
+    else:
+        return JsonResponse({"error": "Bad request structure."}, status=404)
 
 def _post_tasks():
     crawl_bookstore()
